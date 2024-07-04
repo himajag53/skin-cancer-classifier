f4592c6c90a7cb0b8bdf1564cb560e133fa4b35d
Himaja R. Ginkala <hginkala@gmail.com>
2024-07-03 23:05:48 -0400

    Post-processing

---
+++ post_processing.py
@@ -0,0 +1,55 @@
+import cv2
+
+from matplotlib import pyplot as plt
+
+def gaussian_blur(image, kernel_size=(5, 5)):
+    """
+    Applies Gaussian blur to the input image.
+
+    Args:
+        image (numpy.ndarray): Input image to be blurred.
+        kernel_size (tuple): Size of the Gaussian kernel.
+
+    Returns:
+        numpy.ndarray: The blurred image.
+    """
+    blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
+    return blurred_image
+
+def histogram_equalization(image):
+    """
+    Applies histogram equalization to the input image to enhance contrast.
+
+    Args:
+        image (numpy.ndarray): Input image to be processed.
+
+    Returns:
+        numpy.ndarray: Image with enhanced contrast.
+    """
+     # grayscale image
+    if len(image.shape) == 2:
+        equalized_image = cv2.equalizeHist(image)
+
+    # color image
+    elif len(image.shape) == 3:  
+        img_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+        img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+        equalized_image = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+    else:
+        raise ValueError("Image format unsupported!")
+    
+    return equalized_image
+
+def display_image(image, title="Image"):
+    """
+    Displays an image using matplotlib.
+
+    Args:
+        image (numpy.ndarray): Image to be displayed.
+        title (str): Title of the image plot, default is "Image".
+    """
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
